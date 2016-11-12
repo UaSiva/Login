7f4aef3b86fe19f4e9b7a4e59425ab338d63c42e
vijai <vijai96@gmail.com>
2016-11-12 16:45:49 +0530

    added init and register files

--- register.php
+++ register.php
@@ -5,12 +5,6 @@
 	$user_name=$_POST["user_name"];
 	$password=$_POST["password"];
 	
-	/* $name="first_user";
-        $email="first@email.com";
-        $user_name="first_name";
-        $password="first_password";*/
-
-
 	
 	$sql="select * from user_info where email like'".$email."';";
 	
@@ -21,10 +15,9 @@
 		
 		$code="reg failed";$message="user already exits";
 		array_push($response,array("code"=>$code,"message"=>$message));
-<<<<<<< HEAD
-		header('Content-type:application/json');
-=======
->>>>>>> 3dd90d73ed5b9b23699cc10fec2a0c0ed2f8999d
+
+		//header('Content-type:application/json');
+
 		echo json_encode($response);	
 	}
 	else{
@@ -32,10 +25,6 @@
 		  $result=mysqli_query($con,$sql);
 		$code="reg success";$message="now you can login";
                 array_push($response,array("code"=>$code,"message"=>$message));
-<<<<<<< HEAD
-		header('Content-type:application/json');
-=======
->>>>>>> 3dd90d73ed5b9b23699cc10fec2a0c0ed2f8999d
                 echo json_encode($response);
         }
 
